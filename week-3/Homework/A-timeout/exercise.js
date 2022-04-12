2e94daa487e7c307936860dbad98fdddc9fdac1a
JoaoAlb2021 <joaorenato2015@gmail.com>
2022-04-11 23:21:11 -0500

    Upload homework week 3 to 25%

--- week-3/Homework/A-timeout/exercise.js
+++ week-3/Homework/A-timeout/exercise.js
@@ -9,3 +9,15 @@ Task 2
 Update your code to make the colour change every 5 seconds to something different. Hint: try searching for setInterval. (https://www.w3schools.com/jsref/met_win_setinterval.asp)
 ================
 */
+
+setTimeout(function(){
+    document.querySelector('body').style.backgroundColor = 'yellow';
+}, 2000);
+
+setInterval(function () {
+    const randomNumberRed = Math.random()*255
+    const randomNumberGreen = Math.random()*255
+    const randomNumberBlue = Math.random()*255
+    const randomRGBColor = 'rgb('+randomNumberRed+','+randomNumberGreen+','+randomNumberBlue+')'
+    document.querySelector('body').style.backgroundColor = randomRGBColor
+}, 5000);
